6e5d38dc67e04636d120a938aa3b4533148c1816
Mihail Radkov <mihail.radkov@ontotext.com>
2019-08-19 17:22:39 +0300

    Replaced the Dockerized GraphDB with ZIP distribution for Cypress CI step
    Updated the script to download a ZIP distribution of GraphDB free from Onto's NEXUS
    
    Removed custom Docker image for GraphDB and the related Docker compose file.

--- scripts/run-cypress-tests.sh
+++ scripts/run-cypress-tests.sh
@@ -2,26 +2,29 @@
 
 set -eu
 
-# Export the variable to be used in the compose file (test-cypress/docker-compose.yml)
-export GRAPHDB_VERSION=$1
+echo "Working directory: $(pwd)"
 
-echo "Starting Cypress tests against GraphDB version ${GRAPHDB_VERSION}"
+GRAPHDB_VERSION=$1
+GRAPHDB_DOWNLOAD_URL="http://maven.ontotext.com/repository/owlim-releases/com/ontotext/graphdb/graphdb-free/${GRAPHDB_VERSION}/graphdb-free-${GRAPHDB_VERSION}-dist.zip"
 
-# Build Workbench image
-docker build -t graphdb-workbench:latest .
+echo "Downloading GraphDB from $GRAPHDB_DOWNLOAD_URL"
 
-# Build custom GraphDB image with text fixtures and start the compose
-docker-compose --file test-cypress/docker-compose.yml up -d --build
+curl -SL ${GRAPHDB_DOWNLOAD_URL} -o /tmp/graphdb.zip
+unzip -q /tmp/graphdb.zip -d /tmp/graphdb-tmp
+mv /tmp/graphdb-tmp/graphdb* /opt/graphdb
 
-# Wait until both containers are reachable
+echo "Starting GraphDB daemon"
+/opt/graphdb/bin/graphdb -d \
+    -Dgraphdb.home=/opt/graphdb \
+    -Dgraphdb.workbench.home=./src/ \
+    -Dgraphdb.workbench.importDirectory=./test-cypress/fixtures/graphdb-import/
+
+echo "Waiting until GraphDB is reachable"
 npm install -g wait-on
 
 # Wait GraphDB for 60 seconds
 wait-on http://localhost:7200 -t 60000
 
-# Wait Workbench for 30 seconds
-wait-on http://localhost:7300 -t 30000
-
-# Run the tests
-npx cypress run --record=false --config baseUrl=http://localhost:7300,video=false
+echo "Starting the tests!"
+npx cypress run --record=false --config baseUrl=http://localhost:7200,video=false
 
